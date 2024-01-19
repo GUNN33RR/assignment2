b258f662903aff48b21dfeedf672ab994b77dff8
GUNN33RR <ghanemismail16@gmail.com>
2024-01-19 12:59:32 -0700

    first commit

---
+++ pages/signup.vue
@@ -0,0 +1,27 @@
+<script setup>
+const supabase = useSupabaseClient();
+const email = ref("");
+const password = ref(null);
+
+async signup(){
+
+  const { data } = await supabase.auth.signUp({
+
+email: email.value,
+password: password.value,
+
+//options: { emai
+
+//}
+
+
+
+  })
+}
+</script>
+
+<template>
+<main>
+<h1>Sign up</h1>
+</main>
+</template>
